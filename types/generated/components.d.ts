f05a31549c2bff604ed7d990e16ad2ce147ebec7
Kawã <kawaalacoque@gmail.com>
2026-05-05 10:22:55 -0300

    feat: cria componentes de dynamic zones para os posts

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,32 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksBlocoDeImagem extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_bloco_de_imagems';
+  info: {
+    displayName: 'bloco-de-imagem';
+    icon: 'picture';
+  };
+  attributes: {
+    img: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+  };
+}
+
+export interface BlocksBlocoDeTexto extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_bloco_de_textos';
+  info: {
+    displayName: 'bloco-de-texto';
+    icon: 'italic';
+  };
+  attributes: {
+    text: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.bloco-de-imagem': BlocksBlocoDeImagem;
+      'blocks.bloco-de-texto': BlocksBlocoDeTexto;
+    }
+  }
+}
